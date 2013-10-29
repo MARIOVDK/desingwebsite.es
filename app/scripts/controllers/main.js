a9968c3cf1024d1a38387e92bcdecdb4d3e7087d
Mario Fuentes <mariofuentes@macbook.local>
2013-10-29 20:19:10 +0100

    mensajito....

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mywebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
